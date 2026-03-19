66cab53db29aa262ca83fa3b2e289f7a37a46366
ddieperi <david.dieperink@ik.me>
2026-03-19 12:04:24 +0100

    feat: added new properties to main application

--- src/main/java/com/example/cacheservice/CacheserviceApplication.java
+++ src/main/java/com/example/cacheservice/CacheserviceApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.example.cacheservice.cache.config.CacheOperationProperties;
 import com.example.cacheservice.cache.config.CacheRedisProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(CacheRedisProperties.class)
+@EnableConfigurationProperties({ CacheRedisProperties.class, CacheOperationProperties.class })
 public class CacheserviceApplication {
 
 	public static void main(String[] args) {
