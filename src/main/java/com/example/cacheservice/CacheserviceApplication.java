c3c2b384804745e72676fc1f0d15d4b03a052b0c
ddieperi <david.dieperink@ik.me>
2026-03-19 09:22:14 +0100

    feat: updated Application with redis properties

--- src/main/java/com/example/cacheservice/CacheserviceApplication.java
+++ src/main/java/com/example/cacheservice/CacheserviceApplication.java
@@ -2,8 +2,12 @@ package com.example.cacheservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.cacheservice.cache.config.CacheRedisProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CacheRedisProperties.class)
 public class CacheserviceApplication {
 
 	public static void main(String[] args) {
